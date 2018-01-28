4912f0882516fa6a4f8170cdcb49b24963f196da
mariaangelamagtoto.mvp <mariaangelamagtoto.mvp@gmail.com>
2018-01-28 17:01:11 +0800

    Adding the user name information
    - added package-lock.json
    - displayed user first name and last name
    - capitalized user name
    - connected with userinfo api to get user last name and first name
    - displayed first name on the navbar
    - used get info api
    - capitalized user last name
    - added getuserinfo api

--- src/app/shared/app-navbar/app-navbar.component.ts
+++ src/app/shared/app-navbar/app-navbar.component.ts
@@ -1,59 +1,71 @@
-import {Component, Input, OnInit} from "@angular/core";
-import {UserService, NotificationService} from "../../../services/services";
-import {NewMessageModalComponent} from '../new-message-modal/new-message-modal.component';
+import { Component, Input, OnInit } from "@angular/core";
+import { UserService, NotificationService, AccountSettingService } from "../../../services/services";
+import { NewMessageModalComponent } from '../new-message-modal/new-message-modal.component';
 import * as $ from 'jquery';
 
 @Component({
-    selector: "app-navbar",
-    templateUrl: "./app-navbar.component.html",
-    styleUrls: ["./app-navbar.component.scss"]
+  selector    : "app-navbar",
+  templateUrl : "./app-navbar.component.html",
+  styleUrls   : ["./app-navbar.component.scss"]
 })
 export class AppNavBarComponent implements OnInit {
+  @Input("active") protected active: string;
+  notifications    : any[] = [];
+  messages         : any[] = [];
+  userMenuOpen     = false;
+  searchOpen       = false;
+  messagesOpen     = false;
+  notificationOpen = false;
+  userData         = null;
 
-    @Input("active") protected active: string;
-    notifications: any[] = [];
-    messages: any[] = [];
-    userMenuOpen = false;
-    searchOpen = false;
-    messagesOpen = false;
-    notificationOpen = false;
-    constructor(
-      private _userService: UserService,
-      private _notificationservice: NotificationService
-    ) {
-        //console.log(this._userService.getLoggedInUser());
-    }
+  constructor(
+    private _userService: UserService,
+    private _accountsettingservice: AccountSettingService,
+    private _notificationservice: NotificationService
+  ) { //console.log(this._userService.getLoggedInUser());
+  }
 
-    ngOnInit() {
-        this._notificationservice.getNotifications(1, 5).subscribe(resp => {
-            if (resp["error"] === false) {
-                this.notifications = resp["Notifications"];
-            }
-        }, error => {
-            console.log(error);
-        });
-        this._notificationservice.getMessages(1, 5).subscribe(resp => {
-            //console.log(resp);
-            if (resp["error"] === false) {
-                this.messages = resp["Messages"];
-            }
-        }, error => {
-            console.log(error);
-        });
+  ngOnInit() {
+    this.getUserInfo();
+    this._notificationservice.getNotifications(1, 5).subscribe(resp => {
+      if (resp["error"] === false) {
+        this.notifications = resp["Notifications"];
+      }
+    }, error => {
+      console.log(error);
+    });
+    this._notificationservice.getMessages(1, 5).subscribe(resp => {
+      if (resp["error"] === false) {
+        this.messages = resp["Messages"];
+      }
+    }, error => {
+      console.log(error);
+    });
+    $('body')
+      .removeClass('_bg_white')
+      .addClass('_bg_gray');
+  }
 
-        $('body')
-          .removeClass('_bg_white')
-          .addClass('_bg_gray');
-    }
+  openRightMenu(variable) {
+    const oldCondition    = this[variable];
+    this.userMenuOpen     = false;
+    this.searchOpen       = false;
+    this.messagesOpen     = false;
+    this.notificationOpen = false;
+    this[variable]        = !oldCondition;
+  }
 
-    openRightMenu(variable) {
-      const oldCondition = this[variable];
-
-      this.userMenuOpen = false;
-      this.searchOpen = false;
-      this.messagesOpen = false;
-      this.notificationOpen = false;
-
-      this[variable] = !oldCondition;
-    }
+  getUserInfo() {
+    let user     = localStorage.getItem("user");
+    let userInfo = JSON.parse(user);
+    let userId   = userInfo.id;
+    this._accountsettingservice.getUserInfo(userId).subscribe(response => {
+      this.userData    = response["user"];
+      if (response["error"] === false) {
+        alert(response["Message"]);
+      }
+    }, error => {
+      console.log(error);
+    })
+  }
 }
