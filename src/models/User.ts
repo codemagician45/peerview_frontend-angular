6e0245415cbf350cdf1652d155596acf18aac348
Jo-Ries Canino <canino_jories@hotmail.com>
2018-02-09 18:49:38 +0800

    Add User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,7 @@
+import {
+  Course
+} from './Course';
+
 export class User {
   constructor (
     public firstname: String,
@@ -23,4 +27,35 @@ export class UserOnboardingDetails {
   public institutionName: string;
   public yearOfIncorporation: string;
   public website: string;
+}
+
+export class UserModel {
+  public id: number;
+  public firstName?: string;
+  public lastname: string;
+  public password: string;
+  public email: string;
+  public language: string;
+  public aboutMe: string;
+  public accomplishments: string;
+  public token: string;
+  public tokenActiveDate: Date;
+  public isSuspended: boolean;
+  public profilePicture: string;
+  public profilePrivacy: boolean;
+  public protectPost: boolean;
+  public userPrivacyId: number;
+  public facebookId: string;
+  public linkedinId: string;
+  public googleId: string;
+  public schoolName: string;
+  public birthDate: Date;
+  public city: string;
+  public gender: string;
+  public role: string;
+  public company: string;
+  public institutionName: string;
+  public yearOfIncorporation: Date;
+  public website: string;
+  public course: Course;
 }
