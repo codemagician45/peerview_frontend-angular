da70af137521075b4340d0024d57292cb427e171
Jo-Ries Canino <canino_jories@hotmail.com>
2018-02-27 21:22:09 +0800

    Add development config

---
+++ src/config/development.ts
@@ -0,0 +1,15 @@
+export const DEVELOPMENT = {
+  api: 'http://localhost:3000/api/v1/',
+  socialProviders: {
+    'google': {
+      'clientId': '957178873235-f2vj3q02hjm0d0sgntj3ttamuqr3cg2t.apps.googleusercontent.com'
+    },
+    'linkedin': {
+      'clientId': '77y99ina3fva1b'
+    },
+    'facebook': {
+      'clientId': '2014589065490082',
+      'apiVersion': 'v2.11'
+    }
+  }
+};
