80178943885ea9138249561c7754f9d87996000f
Jo-Ries Canino <canino_jories@hotmail.com>
2018-03-03 12:45:39 +0800

    Add on-boarding routes and component

---
+++ src/app/user/on-boarding/on-boarding.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'user-on-boarding-component',
+  templateUrl: './on-boarding.component.html',
+  styleUrls: ['./on-boarding.component.scss']
+})
+export class UserOnboardingComponent {
+  constructor () {}
+
+}
---
+++ src/app/user/on-boarding/select-status/select-status.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'user-on-boarding-select-status-component',
+  templateUrl: './select-status.component.html',
+  styleUrls: ['./select-status.component.scss']
+})
+export class UserOnboardingSelectStatusComponent {
+  constructor () {}
+
+}
