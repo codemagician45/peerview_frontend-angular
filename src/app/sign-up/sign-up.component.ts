f874535d0e803c0d95850efe45bf8917a1e0f142
Lorence Tolentino <rhenztolentino@gmail.com>
2018-03-04 20:04:57 +0800

    Updated refactor signup task

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -2,12 +2,22 @@ import {
   Component
 } from '@angular/core';
 import {
+  Router
+} from '@angular/router';
+import {
   UserModel,
+  SignInViaSocialModel,
+  ISocialResponse,
+  ISignInViaSocialResponse,
   Response
 } from '../shared/models';
 import {
   UserService
 } from  '../../services';
+import {
+  AuthService
+} from 'angular2-social-login';
+
 
 @Component({
   selector: 'sign-up-component',
@@ -15,10 +25,15 @@ import {
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
-  constructor (private userService: UserService) {}
+  constructor (
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   protected hasAgreed: boolean = false;
   protected user: UserModel = new UserModel();
+  private signInViaSocial: SignInViaSocialModel = new SignInViaSocialModel();
 
   protected onSignUp (): void {
     const splitNames = this.user.name.split(' ');
@@ -29,6 +44,41 @@ export class SignUpComponent {
     .subscribe((response: Response) => {
       console.log(response);
     }, (error) => {
+    });
+  }
+
+  protected onSignUpViaSocial (provider: string): void {
+    this.authService.login(provider)
+    .flatMap((response: ISocialResponse) => {
+      const name = response.name.split(' ');
+      this.signInViaSocial.email = response.email;
+      this.signInViaSocial.firstName = name[0];
+      this.signInViaSocial.lastName = name[1];
+      this.signInViaSocial.image = response.image;
+      this.signInViaSocial.provider = response.provider;
+      this.signInViaSocial.uid = response.uid;
+      return this.userService.signInViaSocial(this.signInViaSocial);
+    })
+    .flatMap((response: ISignInViaSocialResponse) => {
+      return this.userService.setLoggedInUser(response.user);
+    })
+    .subscribe(() => {
+      this.router.navigate(['/home']);
+    }, (error) => {
+
+    });
+  }
+
+  private registerCustomer (user): void {
+    this.userService.signUp(user).subscribe((resp) => {
+      console.log(resp);
+      if (resp['status'] === 'SUCCESS') {
+        // this.submitted = true;
+        // this.router.navigate(['/onBoard']);
+      }
+    }, (error) => {
+      console.log(error);
+      // this.responceError = error['error'].status_message;
     });
   }
 }
