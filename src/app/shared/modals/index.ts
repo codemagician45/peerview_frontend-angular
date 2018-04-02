90a2225ee2f15d9481ce319ab71be758a1ac5d8f
Jo-Ries Canino <canino_jories@hotmail.com>
2018-04-02 09:09:43 +0800

    Add a newline

--- src/app/shared/modals/index.ts
+++ src/app/shared/modals/index.ts
@@ -2,3 +2,4 @@ export * from './post-detail/post-detail.component';
 export * from './share-post/share.post.modal.component';
 export * from './view-post/view.post.modal.component';
 export * from './report-post/report.post.modal.component';
+export * from './confirm/confirm-modal.component';
