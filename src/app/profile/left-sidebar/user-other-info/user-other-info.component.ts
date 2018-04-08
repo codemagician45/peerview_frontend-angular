a4f69b9ea8d895222c917ea57ce36a776e370a8d
Jo-Ries Canino <canino_jories@hotmail.com>
2018-04-08 21:15:38 +0800

    UI Changes
    - Includes Post UI
    - Includes User Info Details
    - Includes Social component
    - Includes Shared components

--- src/app/profile/left-sidebar/user-other-info/user-other-info.component.ts
+++ src/app/profile/left-sidebar/user-other-info/user-other-info.component.ts
@@ -1,20 +1,50 @@
 import {
-  Component
+  Component,
+  OnInit
 } from '@angular/core';
 import {
-  UserModel
+  ActivatedRoute
+} from '@angular/router';
+import {
+  UserModel,
+  UserResponse
 } from '../../../shared/models';
 import {
   UserClass
 } from '../../../shared/classes';
+import {
+  UserService
+} from '../../../../services';
+import {
+  CryptoUtilities
+} from '../../../shared/utilities';
 
 @Component({
   selector: 'profile-left-sidebar-user-other-info-component',
   templateUrl: './user-other-info.component.html',
   styleUrls: ['./user-other-info.component.scss']
 })
-export class ProfileLeftSidebarUserOtherInfoComponent {
-  constructor () {}
+export class ProfileLeftSidebarUserOtherInfoComponent implements OnInit {
+  constructor (
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {
+    this.route.params.subscribe(params => {
+      this.userId = params.id;
+      if (this.userId) { this.userId = this.cryptoUtilities.decipher(this.userId); }
+    });
+  }
 
-  protected user: UserModel = UserClass.getUser();
+  protected user: UserModel;
+  private userId: string;
+  private cryptoUtilities = new CryptoUtilities();
+
+  public ngOnInit (): void {
+    this.userId && this.userService.getProfile(this.userId)
+    .subscribe((response: UserResponse) => {
+      this.user = response.user;
+    });
+
+    !this.userId && (this.user = UserClass.getUser());
+  }
 }
