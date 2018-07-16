796e8c5098732620d4b881e8e748a59c6e6d85be
Jo-Ries Canino <canino_jories@hotmail.com>
2018-07-16 20:34:19 +0800

    Add static message notification service

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,3 +4,4 @@ export * from './post.service';
 export * from './course.service';
 export * from './interest.service';
 export * from './utilities.service';
+export * from './message-notification.service';
