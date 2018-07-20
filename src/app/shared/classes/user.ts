a3b3b9d1493140ea9e62806f7eed6970793eeb68
Jo-Ries Canino <canino_jories@hotmail.com>
2018-07-21 03:53:13 +0800

    Remove console.logs

--- src/app/shared/classes/user.ts
+++ src/app/shared/classes/user.ts
@@ -6,8 +6,6 @@ export class UserClass {
   private static user: UserModel;
 
   public static setUser (user: UserModel): void {
-    console.log('setUser');
-    console.log(user);
     UserClass.user = user;
   }
 
