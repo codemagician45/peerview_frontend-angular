00d4efe3086e2a4a739c2301d04f791614cb16b5
Jo-Ries Canino <canino_jories@hotmail.com>
2018-08-04 09:52:36 +0800

    Add CanActivateUserProfile in campus route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ export const appRoutes: Routes = [ {
 }, {
   path: 'campus',
   loadChildren: './campus/campus.module#CampusModule',
+  canActivate: [CanActivateUserProfile],
   data: { state: 'campus' }
 }, {
   path: 'community',
