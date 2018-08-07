995996ec52cbbb0aeb9e4dff031f8036176f460c
Lorence Tolentino <rhenztolentino@gmail.com>
2018-08-07 23:18:05 +0800

    Added Follow User Event

--- src/app/profile/right-sidebar/right-sidebar.component.ts
+++ src/app/profile/right-sidebar/right-sidebar.component.ts
@@ -1,6 +1,20 @@
 import {
   Component
 } from '@angular/core';
+import {
+  UserApiService
+} from '../../../services/api/user.api.service';
+import {
+  UserService
+} from '../../../services/user.service';
+import {
+  UserModel,
+  IResponse
+} from '../../shared/models';
+import {
+  MessageNotificationService,
+  NotificationTypes
+} from '../../../services';
 
 @Component({
   selector: 'profile-right-sidebar-component',
@@ -8,5 +22,31 @@ import {
   styleUrls: ['./right-sidebar.component.scss']
 })
 export class ProfileRightSidebarComponent {
-  constructor () {}
+  constructor (private userApiService: UserApiService) {}
+
+  protected user: UserModel = UserService.getOtherUser();
+  protected followed: boolean = false;
+
+  protected onClickFollowButton (): void {
+    this.userApiService.promisePostFollowUser(this.user.id)
+      .then((response: IResponse) => {
+        console.log('follow user', response);
+        this.followed = true;
+      })
+      .catch(error => {
+        console.log('follow user', error);
+        this.followErrorNotification();
+      });
+  }
+
+  private followErrorNotification (): MessageNotificationService {
+    return MessageNotificationService.show({
+      notification: {
+        id: 'right-sidebar-follow-message',
+        message: 'Follow Error',
+        instruction: 'Something went wrong! Please try again.'
+      }
+    },
+    NotificationTypes.Error);
+  }
 }
