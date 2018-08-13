5a9fb6c1256ae4d44ec561c92654a0a427dfe2db
mariaangelamagtoto <mariaangelamagtoto@gmail.com>
2018-08-13 23:23:20 +0800

    Removed console

--- src/app/shared/components/peers-you-may-know/peers-you-may-know.component.ts
+++ src/app/shared/components/peers-you-may-know/peers-you-may-know.component.ts
@@ -25,7 +25,6 @@ export class SharedPeersYouMayKnowComponent {
     this.userApiService.promiseGetPeersList()
       .then((users: UserModel[]) => {
         this.peers = users;
-        console.log(this.peers);
       })
       .catch(() => {});
   }
