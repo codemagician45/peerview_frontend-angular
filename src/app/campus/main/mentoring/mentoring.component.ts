73a3b7282756dff1bef9d0a97aaa2d7cd01a3061
mariaangelamagtoto <mariaangelamagtoto@gmail.com>
2018-08-16 23:47:59 +0800

    Placed dummy data for mentors

--- src/app/campus/main/mentoring/mentoring.component.ts
+++ src/app/campus/main/mentoring/mentoring.component.ts
@@ -10,8 +10,13 @@ import {
   Overlay
 } from '@angular/cdk/overlay';
 import {
-	BecomeMentorModalComponent
+	BecomeMentorModalComponent,
+	MentorModalComponent
 } from '../../../shared/modals';
+import {
+	MentorModel
+} from '../../../shared/models';
+
 @Component({
 	selector: 'campus-mentoring-component',
 	templateUrl: './mentoring.component.html',
@@ -23,17 +28,43 @@ export class CampusMentoringComponent {
     private overlay: Overlay
 	) {}
 
-	protected sampleMentorsList = [
-		{ name: 'Paul Molive'},
-		{ name: 'Anna Mull'},
-		{ name: 'Gail Forcewind'},
-	];
+	protected mentors = [{
+		id: 1,
+		firstName: 'Paul',
+		lastName: 'Molive',
+		expertise: 'Economics, Arts',
+		contactDetals: '0288172938',
+		reasonForBecomingAMentor: 'I find mentoring fulfilling.'
+	}, {
+		id: 2,
+		firstName: 'Anna',
+		lastName: 'Wayne',
+		expertise: 'Mathematics, Science',
+		contactDetals: '028835485',
+		reasonForBecomingAMentor: 'Helping has been my passion.'
+	}, {
+		id: 3,
+		firstName: 'Gail',
+		lastName: 'Forcewind',
+		expertise: 'Literature, Programming',
+		contactDetals: '0241285',
+		reasonForBecomingAMentor: 'Knowledge is like a lamp, let it shine upon other.'
+	}];
+
+	public ngOnInit (): void {}
 
 	protected onBecomeAMentor (): void {
     let dialogConfig = new MatDialogConfig();
 		dialogConfig.panelClass = 'become-a-mentor-modal';
     dialogConfig.scrollStrategy = this.overlay.scrollStrategies.block();
-
 		this.dialog.open(BecomeMentorModalComponent, dialogConfig);
+	}
+
+	protected onClickMentorName (data): void {
+		let dialogConfig = new MatDialogConfig();
+		dialogConfig.panelClass = 'mentor-details-modal';
+    dialogConfig.scrollStrategy = this.overlay.scrollStrategies.block();
+		dialogConfig.data = {data};
+		this.dialog.open(MentorModalComponent, dialogConfig);
 	}
 }
