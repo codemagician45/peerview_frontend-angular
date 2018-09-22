af77d6b38ce97de461c969d535ced945e2db84d5
mariaangelamagtoto <mariaangelamagtoto@gmail.com>
2018-09-23 01:07:35 +0800

    Adjusting confirm modal

--- src/app/shared/components/report-post/report.post.component.ts
+++ src/app/shared/components/report-post/report.post.component.ts
@@ -5,8 +5,12 @@ import {
   EventEmitter
 } from '@angular/core';
 import {
-  MatDialog
+  MatDialog,
+  MatDialogConfig
 } from '@angular/material';
+import {
+  Overlay
+} from '@angular/cdk/overlay';
 import {
   ReportPostModalComponent,
   SharedConfirmModalComponent
@@ -14,13 +18,17 @@ import {
 import {
   PostModel,
 } from '../../models';
+
 @Component({
   selector: 'report-post-component',
   templateUrl: './report.post.component.html',
   styleUrls: ['./report.post.component.scss']
 })
 export class SharedReportPostComponent {
-  constructor (public dialog: MatDialog) {}
+  constructor (
+    private dialog: MatDialog,
+    private overlay: Overlay
+  ) {}
   @Output() protected onDeletePost = new EventEmitter();
   @Input() protected post: PostModel;
 
@@ -32,10 +40,15 @@ export class SharedReportPostComponent {
   }
 
   protected onOpenConfirmModal (): void {
-    this.dialog.open(SharedConfirmModalComponent, {
-      data: this.post,
-      id: 'SharedConfirmModalComponent'
-    })
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.panelClass = 'image-preview-modal';
+    dialogConfig.disableClose = true;
+    dialogConfig.scrollStrategy = this.overlay.scrollStrategies.block();
+    dialogConfig.id = 'SharedConfirmModalComponent';
+    dialogConfig.data = {
+     description : 'Are you sure you want to cancel?'
+    };
+    this.dialog.open(SharedConfirmModalComponent, dialogConfig)
     .beforeClose()
     .subscribe(response => {
       this.onDeletePost.emit(this.post.id);
