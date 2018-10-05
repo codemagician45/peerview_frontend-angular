488fdae163845b84afbb06b335346a8935c470fc
Lorence Tolentino <rhenztolentino@gmail.com>
2018-10-06 00:31:59 +0800

    Notification modification   - Notification Modal and Page UI Adjustments   - Notification UI Adjustments

---
+++ src/app/shared/components/notifcation-list/notification-list.component.ts
@@ -0,0 +1,14 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'notification-list-component',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss']
+})
+export class SharedNotificationListComponent {
+  constructor () {}
+
+
+}
--- src/app/shared/components/shared.module.ts
+++ src/app/shared/components/shared.module.ts
@@ -86,6 +86,9 @@ import {
   SharedAnnouncementComponent
 } from './announcement/announcement.component';
 import {
+  SharedNotificationListComponent
+} from './notifcation-list/notification-list.component';
+import {
   TimeAgoPipe
 } from 'time-ago-pipe';
 /*providers*/
@@ -139,6 +142,7 @@ export function provideConfig (): any {
     SharedUploadImageComponent,
     SharedReportPostComponent,
     SharedAnnouncementComponent,
+    SharedNotificationListComponent,
     IndexFooterComponent,
     TimeAgoPipe
   ],
@@ -167,6 +171,7 @@ export function provideConfig (): any {
     MatProgressBarModule,
     MatDatepickerModule,
     SharedAnnouncementComponent,
+    SharedNotificationListComponent,
     IndexFooterComponent,
     MatInputModule
   ],
