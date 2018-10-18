b2d52361181554ec7bcd8711e36145c477e14cc1
Jo-Ries Canino <canino_jories@hotmail.com>
2018-10-19 07:58:01 +0800

    Add metaservice and titleservice in index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,5 @@ export * from './utilities.service';
 export * from './message-notification.service';
 export * from './token-store.service';
 export * from './user.service';
+export * from './title.service';
+export * from './meta.service';
