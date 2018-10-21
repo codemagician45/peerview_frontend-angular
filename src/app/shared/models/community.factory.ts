1c0dcfccc0067c802157e50d4196c1bc3cd1a02d
mariaangelamagtoto <mariaangelamagtoto@gmail.com>
2018-10-21 22:19:34 +0800

    Placed create private community factory

--- src/app/shared/models/community.factory.ts
+++ src/app/shared/models/community.factory.ts
@@ -1,10 +1,16 @@
 import {
-  CommunityPostModel
+	CommunityPostModel,
+	PrivateCommunityModel
 } from './community';
 
 export class CommunityFactory {
 	public static createCommunityPost (data: any): CommunityPostModel {
 		return <CommunityPostModel> (new CommunityPostModel())
+			.assimilate(data);
+	}
+
+	public static createPrivateCommunity (data: any): PrivateCommunityModel {
+		return <PrivateCommunityModel> (new PrivateCommunityModel())
 			.assimilate(data);
 	}
 }
