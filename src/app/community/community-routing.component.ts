c723164797215731792a48ecfe49bca57fcfec25
mariaangelamagtoto <mariaangelamagtoto@gmail.com>
2018-11-22 00:02:36 +0800

    Added Answer question component

--- src/app/community/community-routing.component.ts
+++ src/app/community/community-routing.component.ts
@@ -10,17 +10,31 @@ import {
 } from './community.component';
 import {
   StudentCommunityComponent
-} from './student-community/student-community.component';
+} from './student-community/landing/student-community.component';
 import {
   PrivateCommunityComponent
 } from './private-community/private-community.component';
+import {
+  AnswerQuestionCommunityComponent
+} from './student-community/answer-question/answer-question.component';
 
 const communityRoutes: Routes = [{
   path: '',
   component: CommunityComponent,
   children: [ {
     path: 'student-community',
-    component: StudentCommunityComponent
+    component: StudentCommunityComponent,
+    children: [{
+      path: '',
+      redirectTo: 'landing',
+      pathMatch: 'full',
+    },{
+      path: 'landing',
+      component: StudentCommunityComponent
+    },{
+      path: ':id',
+      component:  AnswerQuestionCommunityComponent
+    }]
   }, {
     path: 'private-community',
     component: PrivateCommunityComponent
