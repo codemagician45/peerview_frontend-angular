dbb5deee3d0243db2e6c7e00303de5016f2f3dad
mariaangelamagtoto <mariaangelamagtoto@gmail.com>
2018-11-23 00:00:49 +0800

    Adjusted params for student community

--- src/app/community/student-community/answer-question/answer-question.component.ts
+++ src/app/community/student-community/answer-question/answer-question.component.ts
@@ -1,25 +1,32 @@
 import {
-    Component
+  Component
 } from '@angular/core';
 import {
-    ActivatedRoute,
-    Router,
-    Params
+	ActivatedRoute,
+	Router,
+	Params
 } from '@angular/router';
+import {
+	CryptoUtilities
+} from '../../../shared/utilities';
 
 @Component({
-    selector: 'answer-question-component',
-    templateUrl: './answer-question.component.html',
-    styleUrls: ['./answer-question.component.scss']
+	selector: 'answer-question-component',
+	templateUrl: './answer-question.component.html',
+	styleUrls: ['./answer-question.component.scss']
 })
 export class  AnswerQuestionCommunityComponent {
-    constructor (
-        private route: ActivatedRoute,
-        private router: Router,
-    ) {}
+	constructor (
+			private route: ActivatedRoute,
+			private router: Router,
+	) {}
 
-    public ngOnInit (): void {
-        this.route.parent.params.subscribe((params) => {});
-    }
+	public ngOnInit (): void {
+		this.route.params.subscribe((params) => {
+			let questionId = parseInt(CryptoUtilities.decipher(params.id), 10);
+		});
+	}
+
+	private getQuestionDetails (): void {}
 }
 
